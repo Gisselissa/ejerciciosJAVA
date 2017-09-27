5a3589a97cf37ed3b79366318cdb4229125f0f1f
Sebastián Urbina <sebaurbina@gmail.com>
2017-09-27 16:46:18 -0300

    Commit 2
    Enunciados resueltos.

--- InfoNumerica.java
+++ InfoNumerica.java
@@ -58,27 +58,48 @@ public class InfoNumerica {
         3-Informar si es par. 
         */
         
-        /*1
+        //1- Informar si el número es mayor o menor a 100
+        /*
         if (NumeroIngresado>100) {
             System.out.println("Su número es mayor a 100");
         }
         else{
-            System.out.println("Su número es menor a 100");
+            if (NumeroIngresado!=100) {
+                System.out.println("Su número es menor a 100");
+            }
         }
         */
         
-        /*2
+        //2- Si es positivo o negativo el número ingresado
+        /*Opción 1
         if (NumeroIngresado>0) {
             System.out.println("Su número es positivo");
         }
         else {
-            System.out.println("Su número es negativo");
+            if (NumeroIngresado!=0) {
+                System.out.println("Su número es negativo");
+            }
+            
+        }
+        */
+        /*Opción 2
+        if (NumeroIngresado!=0) {
+            if (NumeroIngresado>0) {
+                System.out.println("Positivo");
+            }
+            else{
+                System.out.println("Negativo");
+            }
+        }
+        */
+
+        //3-Informar si es par.
+        
+        /*if (NumeroIngresado%2==0) {
+            System.out.println("Su número es par");
         }
         */
         
-        if (NumeroIngresado%2==0) {
-            System.out.println("Su número es par");
-        }
         
     }
     
